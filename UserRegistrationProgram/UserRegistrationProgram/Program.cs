79f72d375038c2dfd5f9917d55bd47b3ba76e20a
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-02-16 17:26:19 +0530

    [Prashant]Add: First name validation

---
+++ UserRegistrationProgram/UserRegistrationProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace UserRegistrationProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistration validation = new UserRegistration();
+            Console.WriteLine("User Registration Using RegEx!\n");
+            Console.WriteLine("Enter First Name : ");
+            string fName = Console.ReadLine();
+            bool validFirstName = validation.validateFirstName(fName);
+            Console.WriteLine("First Name Valid : " + validFirstName);
+        }
+    }
+}
---
+++ UserRegistrationProgram/UserRegistrationProgram/UserRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    public class UserRegistration
+    {
+        public static string REGEX_FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public bool validateFirstName(string fName)
+        {
+            return Regex.IsMatch(fName, REGEX_FIRST_NAME);
+        }
+    }
+}
